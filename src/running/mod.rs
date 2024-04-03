d279ad6e43fa3cfb6d47d06ebd24668c301f7386
Nathaniel Curnick <nathaniel.curnick@gmail.com>
2024-04-03 17:29:36 +0100

    added running mean

--- src/running/mod.rs
+++ src/running/mod.rs
@@ -1,5 +1,8 @@
+// See https://nullbuffer.com/articles/welford_algorithm.html
+
 pub trait Running<T> {
     fn running_sum(self) -> Vec<T>;
+    fn running_mean(self) -> Vec<T>;
 }
 
 macro_rules! impl_running_iterator {
@@ -11,6 +14,21 @@ macro_rules! impl_running_iterator {
                 for val in self {
                     sum += val;
                     out.push(sum);
+                }
+
+                return out;
+            }
+
+            fn running_mean(self) -> Vec<$float> {
+                let mut m = 0.0;
+                let mut k = 1.0;
+
+                let mut out = vec![];
+
+                for m_k in self {
+                    m = m + (*m_k - m) / k;
+                    k += 1.0;
+                    out.push(m);
                 }
 
                 return out;
--- tests/running.rs
+++ tests/running.rs
@@ -8,3 +8,12 @@ fn test_running_sum() {
 
     assert_eq!(running, vec![1.0, 3.0, 6.0]);
 }
+
+#[test]
+fn test_running_mean() {
+    let v = vec![1.0, 2.0, 3.0, 4.0];
+
+    let running = v.iter().running_mean();
+
+    assert_eq!(running, vec![1.0, 1.5, 2.0, 2.5]);
+}
