1f634fef40421cb60fc492209380522b48efb218
Indigo Curnick <indigocurnick@gmail.com>
2024-05-04 13:13:18 +0100

    iterators for running + top level docs

--- src/running/mod.rs
+++ src/running/mod.rs
@@ -1,67 +1,131 @@
 // See https://nullbuffer.com/articles/welford_algorithm.html
 
+use num::Float;
+
 // TODO: These would actually be way better as iterators
-pub trait Running<T> {
-    fn running_sum(self) -> Vec<T>;
-    fn running_mean(self) -> Vec<T>;
-    fn running_std(self) -> Vec<T>;
+
+// Running Sum
+pub struct RunningSum<'a, T: Float + 'a, I: Iterator<Item = &'a T>> {
+    iter: I,
+    sum: T,
 }
 
-macro_rules! impl_running_iterator {
-    ($float:ty) => {
-        impl<'a, T: Iterator<Item = &'a $float>> Running<$float> for T {
-            fn running_sum(self) -> Vec<$float> {
-                let mut sum = 0.0;
-                let mut out = vec![];
-                for val in self {
-                    sum += val;
-                    out.push(sum);
-                }
+impl<'a, T: Float + 'a, I: Iterator<Item = &'a T>> RunningSum<'a, T, I> {
+    pub fn new(iter: I) -> Self {
+        return Self {
+            iter: iter,
+            sum: T::zero(),
+        };
+    }
+}
 
-                return out;
+impl<'a, T: Float + 'a, I: Iterator<Item = &'a T>> Iterator for RunningSum<'a, T, I> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.iter.next() {
+            Some(x) => {
+                self.sum = self.sum + *x;
+
+                return Some(self.sum);
             }
-
-            fn running_mean(self) -> Vec<$float> {
-                let mut m = 0.0;
-                let mut k = 1.0;
-
-                let mut out = vec![];
-
-                for m_k in self {
-                    m = m + (*m_k - m) / k;
-                    k += 1.0;
-                    out.push(m);
-                }
-
-                return out;
-            }
-
-            fn running_std(self) -> Vec<$float> {
-                let mut n = 0.0;
-                let mut mean = 0.0;
-                let mut m2 = 0.0;
-
-                let mut stds = vec![];
-
-                for x in self {
-                    n += 1.0;
-                    let delta = x - mean;
-                    mean += delta / n;
-                    let delta2 = x - mean;
-                    m2 += delta * delta2;
-
-                    if n < 2.0 {
-                        stds.push(0.0);
-                    } else {
-                        stds.push((m2 / (n - 1.0)).sqrt());
-                    }
-                }
-
-                return stds;
-            }
+            None => return None,
         }
-    };
+    }
 }
 
-impl_running_iterator!(f64);
-impl_running_iterator!(f32);
+// Running Mean
+pub struct RunningMean<'a, T: Float + 'a, I: Iterator<Item = &'a T>> {
+    iter: I,
+    m: T,
+    k: T,
+}
+
+impl<'a, T: Float + 'a, I: Iterator<Item = &'a T>> RunningMean<'a, T, I> {
+    pub fn new(iter: I) -> Self {
+        return Self {
+            iter,
+            m: T::zero(),
+            k: T::one(),
+        };
+    }
+}
+
+impl<'a, T: Float + 'a, I: Iterator<Item = &'a T>> Iterator for RunningMean<'a, T, I> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.iter.next() {
+            Some(x) => {
+                self.m = self.m + (*x - self.m) / self.k;
+                self.k = self.k + T::one();
+
+                return Some(self.m);
+            }
+            None => return None,
+        }
+    }
+}
+
+// Running Std
+pub struct RunningStd<'a, T: Float + 'a, I: Iterator<Item = &'a T>> {
+    iter: I,
+    n: T,
+    mean: T,
+    m2: T,
+}
+
+impl<'a, T: Float + 'a, I: Iterator<Item = &'a T>> RunningStd<'a, T, I> {
+    pub fn new(iter: I) -> Self {
+        return Self {
+            iter,
+            n: T::zero(),
+            mean: T::zero(),
+            m2: T::zero(),
+        };
+    }
+}
+
+impl<'a, T: Float + 'a, I: Iterator<Item = &'a T>> Iterator for RunningStd<'a, T, I> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.iter.next() {
+            Some(x) => {
+                self.n = self.n + T::one();
+                let delta = *x - self.mean;
+                self.mean = self.mean + (delta / self.n);
+                let delta2 = *x - self.mean;
+                self.m2 = self.m2 + (delta * delta2);
+
+                if self.n < T::from(2.0).unwrap() {
+                    return Some(T::zero());
+                } else {
+                    return Some((self.m2 / (self.n - T::one())).sqrt());
+                }
+            }
+            None => return None,
+        }
+    }
+}
+
+// Use the `Running` trait
+pub trait Running<'a, T: Float + 'a, I: Iterator<Item = &'a T>> {
+    fn running_sum(self) -> RunningSum<'a, T, I>;
+    fn running_mean(self) -> RunningMean<'a, T, I>;
+    fn running_std(self) -> RunningStd<'a, T, I>;
+}
+
+impl<'a, T: Float + 'a, I: Iterator<Item = &'a T>> Running<'a, T, I> for I {
+    fn running_sum(self) -> RunningSum<'a, T, I> {
+        RunningSum::new(self)
+    }
+
+    fn running_mean(self) -> RunningMean<'a, T, I> {
+        RunningMean::new(self)
+    }
+
+    fn running_std(self) -> RunningStd<'a, T, I> {
+        RunningStd::new(self)
+    }
+}
--- tests/running.rs
+++ tests/running.rs
@@ -5,7 +5,7 @@ use vec_utilities::running::Running;
 fn test_running_sum() {
     let v = vec![1.0, 2.0, 3.0];
 
-    let running = v.iter().running_sum();
+    let running: Vec<f64> = v.iter().running_sum().collect();
 
     assert_eq!(running, vec![1.0, 3.0, 6.0]);
 }
@@ -14,7 +14,7 @@ fn test_running_sum() {
 fn test_running_mean() {
     let v = vec![1.0, 2.0, 3.0, 4.0];
 
-    let running = v.iter().running_mean();
+    let running: Vec<f64> = v.iter().running_mean().collect();
 
     assert_eq!(running, vec![1.0, 1.5, 2.0, 2.5]);
 }
@@ -23,11 +23,19 @@ fn test_running_mean() {
 fn test_running_std() {
     let v = vec![1.0, 2.0, 3.0, 4.0];
 
-    let running = v.iter().running_std();
+    let running: Vec<f64> = v.iter().running_std().collect();
     let correct = vec![0.0, 0.70710678118655, 1.0, 1.2909944487358];
-    println!("std {:?}", running);
 
     for (r, c) in running.iter().zip(correct.iter()) {
         assert_approx_eq!(*r, *c, 1e-8);
     }
+}
+
+#[test]
+fn test_running_sum_with_filter() {
+    let v = vec![1.0, 2.0, 3.0];
+
+    let running: Vec<f64> = v.iter().running_sum().filter(|z| *z > 2.0).collect();
+
+    assert_eq!(running, vec![3.0, 6.0]);
 }
